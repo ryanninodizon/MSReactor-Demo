148313705a8c8cc07c8089956912b63adbe9fab0
Ryan Niño Dizon <50763950+ryanninodizon@users.noreply.github.com>
2024-09-07 12:16:07 +0200

    Update index.js

--- AspireRabbitMQ/AspireRabbitMQ.React/src/index.js
+++ AspireRabbitMQ/AspireRabbitMQ.React/src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./components/App";
-
 const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(
