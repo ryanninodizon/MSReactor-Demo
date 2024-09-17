a0af6c288cf3be522b04432ff02128efc8ab0250
Ryan Niño Dizon <ryan.dizon@isdfeniqs.com>
2024-09-17 23:50:58 +0200

    MS Reactor episode 2 Demo - Serverless API

---
+++ ServerlessAPI/FunctionApp/CosmosDbConfig.cs
@@ -0,0 +1,9 @@
+namespace FunctionApp
+{
+    public static class CosmosDbConfig{
+        public static readonly string DbPrimaryKey = "<Add PRimary key>";
+        public static readonly string DbName = "<db name>";
+        public static readonly string DbContainerName = "<container name>";
+        public static readonly string DbEndpointUri = "<Add Endpoint Here>";
+    }
+}
