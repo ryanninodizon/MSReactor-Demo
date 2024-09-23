e27e2d0848e2e16e2dba08509e9d088b0b63d125
Ryan Niño Dizon <50763950+ryanninodizon@users.noreply.github.com>
2024-09-23 13:01:24 +0200

    Update CosmosDbOperations.cs

--- ServerlessAPI/FunctionApp/CosmosDbOperations.cs
+++ ServerlessAPI/FunctionApp/CosmosDbOperations.cs
@@ -17,7 +17,6 @@ namespace FunctionApp
             await response.WriteStringAsync("New Item Added");
             return response;
         }
-
         public static async Task<HttpResponseData> DeleteAsync(HttpRequestData req, Container container, string id)
         {
            await container.DeleteItemAsync<dynamic>(id, new PartitionKey(id));
@@ -25,7 +24,6 @@ namespace FunctionApp
            await response.WriteStringAsync("Item was successfully deleted");
            return response;
         }
-
         public static async Task<HttpResponseData> ReadAsync(HttpRequestData req, Container container, string id)
         {
             try{
@@ -65,7 +63,6 @@ namespace FunctionApp
                 return errorReponse;
             }
         }
-
         public static async Task<HttpResponseData> UpdateAsync(HttpRequestData req, Container container, string id)
         {
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
