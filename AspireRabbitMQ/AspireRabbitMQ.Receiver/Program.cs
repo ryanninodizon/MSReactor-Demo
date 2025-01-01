8b91030977b272e29259a5672373c22c6c3f0c2e
Ryan Niño Dizon <50763950+ryanninodizon@users.noreply.github.com>
2025-01-01 17:47:09 +0100

    Update Program.cs

--- AspireRabbitMQ/AspireRabbitMQ.Receiver/Program.cs
+++ AspireRabbitMQ/AspireRabbitMQ.Receiver/Program.cs
@@ -3,5 +3,6 @@ builder.AddServiceDefaults();
 builder.AddRabbitMQClient("messaging");
 // Add services to the container.
 builder.Services.AddHostedService<ProcessRabbitMQMessage>();
+
 var app = builder.Build();
 app.Run();
